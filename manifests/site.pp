549f91421b31079543b7a868779e2c477f710200
nathanhays9999 <nhays@misoenergy.org>
2016-04-26 18:57:36 -0400

    included nginx class

--- manifests/site.pp
+++ manifests/site.pp
@@ -53,6 +53,7 @@ node default {
 #include users
 include skeleton
 include memcached
+include nginx
 #  exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
 #path => '/usr/bin:/usr/local/bin',
 #creates => '/etc/motd',
