cb38ff9656e3bcc11aed77c67c397ec3faf8c2c9
nathanhays9999 <nhays@misoenergy.org>
2016-04-28 13:35:36 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -9,6 +9,8 @@ $group = 'root'
 # $docroot = '/var/www'
 $confdir = '/etc/nginx'
 $logdir = '/var/log/nginx'
+# this will be used if we don't pass in a value
+$default_docroot = '/var/www'
 }
 'windows' : {
 $package = 'nginx-service'
@@ -17,6 +19,8 @@ $group = 'Administrators'
 # $docroot = 'C:/ProgramData/nginx/html'
 $confdir = 'C:/ProgramData/nginx'
 $logdir = 'C:/ProgramData/nginx/logs'
+# this will be used if we don't pass in a value
+$default_docroot = 'C:/ProgramData/nginx/html'
 }
 default : {
 fail("Module ${module_name} is not supported on ${::osfamily}")
